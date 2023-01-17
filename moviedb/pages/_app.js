6af66d666bd37b27d4e3c14a13313fb497873be5
sobrien-eng <sobrien@student.neumont.edu>
2023-01-17 12:56:15 -0700

    refactoring to allow api data

--- moviedb/pages/_app.js
+++ moviedb/pages/_app.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -8,15 +8,9 @@ import {
   View,
   Card,
 } from "@aws-amplify/ui-react";
+import Link from 'next/link';
+import '../styles/global.css'
 
-function App({ signOut }) {
-  return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
