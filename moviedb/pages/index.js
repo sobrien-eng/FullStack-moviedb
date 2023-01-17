f3e319e549c3f576f6498150e8704446c8b76ca1
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2023-01-17 11:21:31 -0700

    navbar

--- moviedb/pages/index.js
+++ moviedb/pages/index.js
@@ -2,17 +2,20 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-
+     
       </Head>
 
       <main>
-        <h1 className="title">
+        {/* <h1 className="title">
           <Link href="/movies/first-movie">first movie</Link>
         </h1>
         <h1 className="title">
@@ -23,7 +26,7 @@ export default function Home() {
         </h1>
         <h1 className="title">
           <Link href="/admindash">admin</Link>
-        </h1>
+        </h1> */}
 
       </main>
 
